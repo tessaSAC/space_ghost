d1ea25a4da086d01a8e667df83b9c67be3bfa001
Harmony D <antancia@gmail.com>
2017-04-29 15:21:00 -0400

    added express sessions

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const http = require('http'),
 	  express = require('express'),
 	  app = express(),
+      session = require('express-session'),
 	  twilio = require('twilio'),
 	  bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'nyan-cat-loves-space' }));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 })
 
 app.post('/', function(req, res) {
+    let smsCount = req.session.counter || 0;
     const twiml = new twilio.TwimlResponse();
     if (req.body.Body == 'hello') {
         twiml.message('Hi!');
