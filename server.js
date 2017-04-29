81aa73f3194b7793a961f3e9d11e72ddc2c281a5
Harmony D <antancia@gmail.com>
2017-04-29 17:59:47 -0400

    Added example scheduling

--- server.js
+++ server.js
@@ -2,7 +2,33 @@ const http = require('http'),
 	  express = require('express'),
 	  app = express(),
 	  twilio = require('twilio'),
-	  bodyParser = require('body-parser');
+	  bodyParser = require('body-parser'),
+      mongoConnectionString = "mongodb://127.0.0.1:27017/agenda",
+      Agenda = require('agenda'),
+      agenda = new Agenda({db: {address: mongoConnectionString}}),
+      config = require('./config');
+
+var client = require('twilio')(config.accountSid, config.authToken);
+
+agenda.define('send text message', function(job) {
+    client.sms.messages.create({
+        to: config.ghostee,
+        from: config.twilioNumber,
+        body: "SPACE SUCCESS",
+    }, function(err, message) {
+        if(err) {
+            console.error('SPACE ERROR!!!!')
+            console.error(err)
+        } else {
+            console.log('Space success')
+        }
+    });
+});
+
+agenda.on('ready', function() {
+  agenda.every('5 seconds', 'send text message');
+  agenda.start();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
