4c3ea8b9ef3a8b3c4a37321e1f3ebdc256b2f4d2
tessaSAC <tessaSAC@users.noreply.github.com>
2017-04-29 17:43:07 -0400

    adds working text message functionality

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const http = require('http'),
 	  express = require('express'),
 	  app = express(),
 	  twilio = require('twilio'),
-	  bodyParser = require('body-parser');
+	  bodyParser = require('body-parser'),
+	  cfg = require('./config.js'),
+	  accountSid = cfg.accountSid,
+	  authToken = cfg.authToken,
+	  casper = cfg.casper,
+	  ghostee = cfg.ghostee,
+	  twilioNumber = cfg.twilioNumber,
+	  client = twilio(accountSid, authToken);
+
+app.use(express.static('./app'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,13 +20,51 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function(req, res) {
-    const twiml = new twilio.TwimlResponse();
-	twiml.message(function() {
-		this.body(req.body.Body);
-	});
+	client.sms.messages.create({
+		to: casper,
+		from: twilioNumber,
+		body: req.body.Body
+	}, function(error, message) {
+		if (!error) {
+		       // The second argument to the callback will contain the information
+		       // sent back by Twilio for the request. In this case, it is the
+		       // information about the text messsage you just sent:
+		       console.log('Success! The SID for this SMS message is:');
+		       console.log(message.sid);
 
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
+		       console.log('Message sent on:');
+		       console.log(message.dateCreated);
+		   } else {
+		       console.log('Oops! There was an error.', message);
+		   }
+	});
+});
+
+app.post('/spaceghost', function(req, res) {
+ //    const twiml = new twilio.TwimlResponse();
+	// twiml.message(function() {
+	// 	this.body('hi');
+	// });
+
+ //    res.writeHead(200, {'Content-Type': 'text/xml'});
+ //    res.end(twiml.toString());
+
+ //    console.log(req.body.textmsg)
+
+	client.sms.messages.create({
+		to: ghostee,
+		from: twilioNumber,
+		body: req.body.textmsg
+	}, function(error, message) {
+		if (!error) {
+		       console.log('Success! The SID for this SMS message is:');
+		       console.log(message.sid);
+		       console.log('Message sent on:');
+		       console.log(message.dateCreated);
+		   } else {
+		       console.log('Oops! There was an error.', message);
+		   }
+	});
 });
 
 app.listen(2001, function () {
