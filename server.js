3aa7de4010d762057cb74a57b062129ac8c25baa
tessaSAC <tessaSAC@users.noreply.github.com>
2017-04-29 14:17:04 -0400

    gets test sms app running

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const express = require('express');
-const app = express();
+const http = require('http'),
+	  express = require('express'),
+	  app = express(),
+	  twilio = require('twilio'),
+	  bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 })
+
+app.post('/', function(req, res) {
+    const twiml = new twilio.TwimlResponse();
+    if (req.body.Body == 'hello') {
+        twiml.message('Hi!');
+    } else if(req.body.Body == 'bye') {
+        twiml.message('Goodbye');
+    } else {
+        twiml.message('No Body param match, Twilio sends this in the request to your server.');
+    }
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
 
 app.listen(2001, function () {
   console.log('Example app listening on port 2001!');
